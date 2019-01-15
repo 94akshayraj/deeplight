207856444b48aaa8af9d641aa1aa9db60add12ce
akhisreelibra <akhisreelibra@gmail.com>
2019-01-16 02:29:04 +0530

    update

--- DocRead/DocRead.py
+++ DocRead/DocRead.py
@@ -1,17 +1,10 @@
-import urllib
 import cv2
 import numpy as np
 import time
 import pytesseract
-from gtts import gTTS
-import os
 
-def DocRead(img):    
-        
+def DocRead(img):
 	imgNp = np.array(bytearray(img),dtype=np.uint8)
-        
-        img = cv2.imdecode(imgNp,-1)
-
-    	ocr=pytesseract.image_to_string(img)
-
-    	return ocr
+	img = cv2.imdecode(imgNp,-1)
+	ocr=pytesseract.image_to_string(img)
+	return ocr
